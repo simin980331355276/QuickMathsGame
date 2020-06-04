3ac2fba7cc9015b4bba2aef6e7b7c3cd8906b15a
ngcheeying <p16007522@student.newinti.edu.my>
2020-06-04 17:59:06 +0800

    Splash screen and home page

---
+++ app/src/main/java/com/example/quickmathsgame/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.quickmathsgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.quickmathsgame.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mPlayBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .replace(R.id.container, MainFragment.newInstance())
+//                    .commitNow();
+//        }
+
+        mPlayBtn = findViewById(R.id.btn_play);
+        mPlayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
